2670df975473483007a249aff778db412281f7dc
snowdev1205 <ishiiyuki122@gmail.com>
2022-08-11 20:30:58 -0500

    create & complete

---
+++ app/gem-bank/src/globals.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const DEFAULTS = {
+  CLUSTER: 'devnet',
+  GEM_BANK_PROG_ID: new PublicKey(
+    'bankHHdqMuaaST4qQk6mkzxGeKPHWmqdgor6Gs8r88m'
+  ),
+};
